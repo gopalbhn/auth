8b5ffa224d676ba3bf862bd0da898dd573262c0d
Gopal Bhandari <gopalbhandari056@gmail.com>
2026-05-29 11:55:02 +0545

    test

--- server/src/controller/userController.js
+++ server/src/controller/userController.js
@@ -82,11 +82,11 @@ res.cookie("token", token, {
 };
 
 const LogOut = async (req, res) => {
-res.clearCookie("token", {
+res.cookie("token", "",{
   httpOnly: true,
-  secure: true,
   sameSite: "none",
-  path: "/",
+  secure: true,
+  expires:new Date(0)
 });
   res.status(200).json({
     success: true,
