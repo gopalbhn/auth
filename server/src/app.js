0d66805e6bebd1e8e6f766c70b0832b7c8355eaf
Gopal Bhandari <gopalbhandari056@gmail.com>
2026-05-29 07:03:44 +0545

    changes

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cookieParser())
 app.use(express.json())
 app.use(cors({
-    origin:["http://localhost:5173"],
+    origin:["http://localhost:5173","https://auth-eight-taupe.vercel.app"],
     credentials:true
 }))
 
